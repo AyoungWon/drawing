3982c76145a9f96af3e4001c3356e501190e360c
AyoungWon <thwn4750@gmail.com>
2021-03-04 17:31:23 +0900

    squere,circle,text func complete

--- app.js
+++ app.js
@@ -1,5 +1,120 @@
+const canvas = document.querySelector('.canvas')
+const context = canvas.getContext("2d")
+const color = document.querySelector('#color-picker')
+const ranges = document.querySelectorAll('.range')
+const strokeRange = document.querySelector('#stroke-range')
+const textRange = document.querySelector('#textInput')
+const saveBtn = document.querySelector('.save')
+const modeBtns = document.querySelector('.mode-btns')
+let mode, prevX,prevY, lastX,lastY
+let painting = false;
+
+const COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+context.fillStyle = "white";
+context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+context.strokeStyle = COLOR;
+context.fillStyle = COLOR;
+context.lineWidth = 2.5;
+
+
 $('#color-picker').spectrum({
   type: "text",
   showAlpha: true,
   showPalette: true,
 });
+
+
+const handleColorClick = () => {
+console.log('a')
+}
+
+const handleSaveClick = () => {
+
+}
+const handleModeClick = (e) => {
+mode = e.target.getAttribute('id')
+console.log(ranges)
+console.log(mode)
+ranges.forEach(range => {
+  range.style.display = "none"
+})
+if(mode === "drawing"){
+  strokeRange.style.display = "block"
+} else if (mode === "text"){
+  textRange.style.display = "block"
+}
+}
+
+const onMouseMove = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    context.beginPath();
+    context.moveTo(x, y);
+  } else {
+    context.lineTo(x, y);
+    context.stroke();
+  }
+}
+const handleCanvasClick = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if(mode === "text" && textRange.value){
+    const loadText = textRange.value
+    context.font = '48pt gothic'
+    context.fillText(loadText, x,y)
+  }
+
+
+}
+const handleCM = () => {
+
+}
+const startPainting = () => {
+
+}
+const stopPainting = () => {
+
+}
+const onMousedown = (e) => {
+  prevX = e.offsetX;
+  prevY = e.offsetY
+if(mode === drawing){
+
+}else if(mode === "squre"){
+
+}
+}
+const onMouseUp = (e) => {
+  lastX = e.offsetX;
+  lastY = e.offsetY
+if(mode === drawing){
+
+}else if(mode === "squre"){
+ context.fillRect(prevX, prevY, lastX - prevX, lastY-prevY)
+}else if(mode === "circle"){
+  context.beginPath()
+  var radius = lastX - prevX
+  context.arc(prevX,prevY,radius,0,Math.PI * 2, true ) 
+  context.fill()
+}
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMousedown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+
+color.addEventListener('click', handleColorClick)
+saveBtn.addEventListener('click', handleSaveClick)
+modeBtns.addEventListener('click', handleModeClick)
