f3b9eb9235fc2465715cc1a3d9339ec5474f050e
AyoungWon <thwn4750@gmail.com>
2021-03-05 15:04:37 +0900

    stroke custom func complete

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const canvas = document.querySelector('.canvas')
 const context = canvas.getContext("2d")
 const color = document.querySelector('#color-picker')
-const colorSelectBtn = document.querySelector('.sp-choose')
+const strokeColor = document.querySelector('#strokeColor')
 const ranges = document.querySelectorAll('.range')
 const strokeRange = document.querySelector('#stroke-range')
 const textSet = document.querySelector('.text-setting')
 const textInput = document.querySelector('#textInput')
 const textSize = document.querySelector('#font-size')
 const saveBtn = document.querySelector('.save')
+const resetBtn = document.querySelector('.reset')
 const modeBtns = document.querySelector('.mode-btns')
+const modes = document.querySelectorAll('.modeBtn')
+const strokeSet = document.querySelector('.stroke-setting')
 let mode, prevX,prevY, lastX,lastY
 let drawing = false;
+let fill = false
 
 const COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
-
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
-
 context.fillStyle = "white";
 context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 context.strokeStyle = COLOR;
 context.fillStyle = COLOR;
 context.lineWidth = 2.5;
 
+$('#strokeColor').spectrum({
+  type: "text",
+  showAlpha: true,
+  showPalette: true,
+});
 
 $('#color-picker').spectrum({
   type: "text",
   showAlpha: true,
   showPalette: true,
 });
+
 
 
 const handleSaveClick = () => {
@@ -40,31 +48,35 @@ const handleSaveClick = () => {
   link.click();
 }
 const handleModeClick = (e) => {
-mode = e.target.getAttribute('id')
-console.log(mode)
-ranges.forEach(range => {
-  range.style.display = "none"
-})
-if(mode === "drawing"){
-  strokeRange.style.display = "block"
-} else if (mode === "text"){
-  textSet.style.display = "flex"
-}
-}
-
-const onMouseMove = (e) => {
-  const x = e.offsetX;
-  const y = e.offsetY;
-  if (!drawing) {
-    context.beginPath();
-    context.moveTo(x, y);
-  } else {
-    context.lineTo(x, y);
-    context.stroke();
+  mode = e.target.getAttribute('id')
+  ranges.forEach(range => {
+    range.style.display = "none"
+  })
+  if(mode !== null){
+    modes.forEach(mode => {
+      mode.style.border = "2px solid rgba(0, 0, 0, 0.2)"
+    })
+    e.target.style.border = "2px solid rgba(0, 0, 0, 0.9)"
+  }
+  if(mode === "drawing"){
+    strokeRange.style.display = "block"
+  } else if (mode === "text"){
+    textSet.style.display = "flex"
+  } else if (mode === "squre" || mode === "circle"){
+    console.log(strokeSet)
+    strokeSet.style.display = "flex"
   }
 }
+
+const handleCM = (e) => {
+  e.preventDefault();
+}
+
+const stopPainting = () => {
+  drawing = false
+}
+
 const handleCanvasClick = (e) => {
-  context.strokeStyle = color.value
   context.fillStyle = color.value
   const x = e.offsetX;
   const y = e.offsetY;
@@ -79,14 +91,18 @@ const handleCanvasClick = (e) => {
   }else if(mode === "fill"){
     context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
-
-
 }
-const handleCM = (e) => {
-  e.preventDefault();
-}
-const stopPainting = () => {
-  drawing = false
+
+const onMouseMove = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!drawing) {
+    context.beginPath();
+    context.moveTo(x, y);
+  } else {
+    context.lineTo(x, y);
+    context.stroke();
+  }
 }
 
 const onMousedown = (e) => {
@@ -101,16 +117,31 @@ const onMousedown = (e) => {
 const onMouseUp = (e) => {
   lastX = e.offsetX;
   lastY = e.offsetY;
-if(mode === "drawing"){
-  drawing = false
-}else if(mode === "squre"){
- context.fillRect(prevX, prevY, lastX - prevX, lastY-prevY)
-}else if(mode === "circle"){
-  context.beginPath()
-  var radius = lastX - prevX
-  context.arc(prevX,prevY,radius,0,Math.PI * 2, true ) 
-  context.fill()
+  if(mode === "drawing"){
+    drawing = false
+  }else if(mode === "squre"){
+    context.strokeStyle = strokeColor.value
+    context.fillRect(prevX, prevY, lastX - prevX, lastY-prevY)
+    context.strokeRect(prevX, prevY, lastX - prevX, lastY-prevY)
+  }else if(mode === "circle"){
+    context.strokeStyle = strokeColor.value
+    context.beginPath()
+    var radius = lastX - prevX
+    context.arc(prevX,prevY,radius,0,Math.PI * 2, true ) 
+    context.fill()
+    context.stroke()
+  }
 }
+
+const handleResetClick = () => {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  context.fillStyle = color.value
+}
+
+const handleRangeChange = (e) => {
+  const size = e.target.value
+  context.lineWidth = size
 }
 
 if (canvas) {
@@ -123,4 +154,6 @@ if (canvas) {
 }
 
 saveBtn.addEventListener('click', handleSaveClick)
+resetBtn.addEventListener('click', handleResetClick)
 modeBtns.addEventListener('click', handleModeClick)
+strokeRange.addEventListener("input", handleRangeChange);
